1b46555a9e880370750058563a21bf186c06d95d
hujia <hujia7812556@gmail.com>
2025-05-26 21:41:32 +0800

    update

--- admin-ui/next.config.js
+++ admin-ui/next.config.js
@@ -1,18 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      // 代理到 http://localhost:18103
-      {
-        source: '/api/v1/:path*',
-        destination: 'http://localhost:18103/v1/:path*',
-      },
-      // 代理到 http://localhost:10081/admin
-      {
-        source: '/admin/v1/:path*',
-        destination: 'http://localhost:10081/admin/v1/:path*',
-      },
-    ]
+    // 只在开发环境中使用代理
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        // 代理到 http://localhost:18103
+        {
+          source: '/api/v1/:path*',
+          destination: 'http://localhost:18103/v1/:path*',
+        },
+        // 代理到 http://localhost:10081/admin
+        {
+          source: '/admin/v1/:path*',
+          destination: 'http://localhost:10081/admin/v1/:path*',
+        },
+      ]
+    }
+    
+    // 生产环境不使用代理
+    return []
   },
 }
 
