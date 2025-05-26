1fde2cfe2b0dc4f0cacb145b393af1f1576cd8ce
hujia <hujia7812556@gmail.com>
2025-05-27 02:53:48 +0800

    delete proxy

--- admin-ui/next.config.js
+++ admin-ui/next.config.js
@@ -1,24 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    // 只在开发环境中使用代理
-    if (process.env.NODE_ENV === 'development') {
-      return [
-        // 代理到 http://localhost:18103
-        {
-          source: '/api/v1/:path*',
-          destination: `${process.env.API_BASE_URL}/v1/:path*`,
-        },
-        // 代理到 http://localhost:10081/admin
-        {
-          source: '/api/admin/v1/:path*',
-          destination: `${process.env.API_GATEWAY_URL}/v1/:path*`,
-        },
-      ]
-    }
-    
-    // 生产环境不使用代理
-    return []
+    // 不使用任何代理
+    return [];
   },
 }
 
