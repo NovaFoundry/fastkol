723c974e0b3f767a08d865bf27d0840ec36fd4b4
hujia <hujia7812556@gmail.com>
2025-05-29 19:47:33 +0800

    update endpoints similar_users

--- fetcher/app/fetchers/twitter.py
+++ fetcher/app/fetchers/twitter.py
@@ -385,38 +385,9 @@ class TwitterFetcher(BaseFetcher):
             }
             
             features = {
-                "rweb_video_screen_enabled": False,
-                "profile_label_improvements_pcf_label_in_post_enabled": True,
-                "rweb_tipjar_consumption_enabled": True,
-                "responsive_web_graphql_exclude_directive_enabled": True,
-                "verified_phone_label_enabled": False,
-                "creator_subscriptions_tweet_preview_api_enabled": True,
-                "responsive_web_graphql_timeline_navigation_enabled": True,
-                "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
-                "premium_content_api_read_enabled": False,
-                "communities_web_enable_tweet_community_results_fetch": True,
-                "c9s_tweet_anatomy_moderator_badge_enabled": True,
-                "responsive_web_grok_analyze_button_fetch_trends_enabled": False,
-                "responsive_web_grok_analyze_post_followups_enabled": True,
-                "responsive_web_jetfuel_frame": False,
-                "responsive_web_grok_share_attachment_enabled": True,
-                "articles_preview_enabled": True,
-                "responsive_web_edit_tweet_api_enabled": True,
-                "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
-                "view_counts_everywhere_api_enabled": True,
-                "longform_notetweets_consumption_enabled": True,
-                "responsive_web_twitter_article_tweet_consumption_enabled": True,
-                "tweet_awards_web_tipping_enabled": False,
-                "responsive_web_grok_analysis_button_from_backend": False,
-                "creator_subscriptions_quote_tweet_preview_enabled": False,
-                "freedom_of_speech_not_reach_fetch_enabled": True,
-                "standardized_nudges_misinfo": True,
-                "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": True,
-                "rweb_video_timestamps_enabled": True,
-                "longform_notetweets_rich_text_read_enabled": True,
-                "longform_notetweets_inline_media_enabled": True,
-                "responsive_web_grok_image_annotation_enabled": True,
-                "responsive_web_enhance_cards_enabled": False
+                "rweb_video_screen_enabled":False,"profile_label_improvements_pcf_label_in_post_enabled":True,"rweb_tipjar_consumption_enabled":True,"verified_phone_label_enabled":False,"creator_subscriptions_tweet_preview_api_enabled":True,"responsive_web_graphql_timeline_navigation_enabled":True,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":False,
+                "premium_content_api_read_enabled":False,"communities_web_enable_tweet_community_results_fetch":True,"c9s_tweet_anatomy_moderator_badge_enabled":True,"responsive_web_grok_analyze_button_fetch_trends_enabled":False,"responsive_web_grok_analyze_post_followups_enabled":True,"responsive_web_jetfuel_frame":False,"responsive_web_grok_share_attachment_enabled":True,"articles_preview_enabled":True,"responsive_web_edit_tweet_api_enabled":True,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":True,"view_counts_everywhere_api_enabled":True,"longform_notetweets_consumption_enabled":True,"responsive_web_twitter_article_tweet_consumption_enabled":True,"tweet_awards_web_tipping_enabled":False,"responsive_web_grok_show_grok_translated_post":False,"responsive_web_grok_analysis_button_from_backend":True,"creator_subscriptions_quote_tweet_preview_enabled":False,"freedom_of_speech_not_reach_fetch_enabled":True,"standardized_nudges_misinfo":True,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":True,
+                "longform_notetweets_rich_text_read_enabled":True,"longform_notetweets_inline_media_enabled":True,"responsive_web_grok_image_annotation_enabled":True,"responsive_web_enhance_cards_enabled":False
             }
             
             # 构建请求 URL
@@ -480,19 +451,20 @@ class TwitterFetcher(BaseFetcher):
                             
                             # 从简介中提取邮箱
                             email_in_bio = await self._extract_email_from_text(bio)
+                            username = result.get('core', {}).get('screen_name', '')
                             
                             user_data = {
                                 "uid": result.get("rest_id", ""),
-                                "username": legacy.get('screen_name', ''),
-                                "nickname": legacy.get('name', ''),
-                                "is_verified": legacy.get('verified', False),
+                                "username": username,
+                                "nickname": result.get('core', {}).get('name', ''),
+                                "is_verified": result.get('is_blue_verified', False),
                                 "followers_count": legacy.get('followers_count', 0),
                                 "following_count": legacy.get('friends_count', 0),
                                 "tweet_count": legacy.get('statuses_count', 0),
                                 "bio": bio,
                                 "email_in_bio": email_in_bio,
-                                "location": legacy.get('location', ''),
-                                "url": f"https://x.com/{legacy.get('screen_name', '')}"
+                                "location": result.get('location', '').get('location', ''),
+                                "url": f"https://x.com/{username}"
                             }
                             similar_users.append(user_data)
             
